165882223fa41b5194e921ea8ac890255d70cf53
eyasu <mesakmegerem@gmail.com>
2023-10-25 12:55:07 -0700

    add swap function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap - This for Swap two int in an array
+ * @swap1: The first int to swap
+ * @swap2: The second int to swap.
+ */
+void swap(int *swap1, int *swap2)
+{
+	int temp;
+
+	temp = *swap1;
+	*swap1 = *swap2;
+	*swap2 = temp;
+}
+
+/**
  * selection_sort - Sort an array of int in ascending order using selection
  * @array: This for array of int.
  * @size: The size of the array.
